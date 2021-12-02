d6780bfb591b89469d5ef862b817dc993ca37bc8
novaldwp <nvldwiptr@gmail.com>
2021-12-03 04:05:54 +0700

    update config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,44 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+	/**
+     * Return a success JSON response.
+     *
+     * @param  string  $message
+     * @param  int|null  $code
+     * @param  array|string|null  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+	public function success(string $message = null, int $code = 200, $data = null)
+	{
+		return response()->json(
+            [
+                'status'    => 1,
+                'message'   => $message,
+                'data'      => $data
+		    ],
+            $code
+        );
+	}
+    /**
+     * Return an error JSON response.
+     *
+     * @param  string  $message
+     * @param  int  $code
+     * @param  array|string|null  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+	public function error(string $message = null, int $code, $data = null)
+	{
+		return response()->json(
+            [
+                'status' => 0,
+                'message' => $message,
+                'data' => $data
+		    ],
+            $code
+        );
+	}
 }
