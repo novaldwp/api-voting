2aaeb4f5c4136d9e25f8d8f54b492b5627fe4641
novaldwp <nvldwiptr@gmail.com>
2021-12-04 00:18:33 +0700

    create new candidates migration

---
+++ database/migrations/2021_12_03_162930_create_candidates_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('email');
+            $table->string('phone');
+            $table->date('dob');
+            $table->text('address');
+            $table->string('vision');
+            $table->string('mission');
+            $table->foreignId('election_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('election_id')->references('id')->on('elections')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
